32f4af1315008af1c946fe81d08320ca3b5733cb
EdouardParis <mail@edouard.paris>
2016-10-07 10:17:09 +0200

    Fix and improve unit testing
    RecordNotFound return always false with item array
    BindJSON instead of Bind for binding checking

--- task/task.go
+++ task/task.go
@@ -7,12 +7,12 @@ import (
 )
 
 type Task struct {
-	ID          uint   `gorm:"primary_key"`
-	Name        string `binding:"required"`
-	Description string
-	Progression int
-	CreatedAt   time.Time
-	UpdatedAt   time.Time
+	ID          uint      `gorm:"primary_key"`
+	Name        string    `json:"name" binding:"required"`
+	Description string    `json:"description"`
+	Progression int       `json:"progression"`
+	CreatedAt   time.Time `json:"created_at"`
+	UpdatedAt   time.Time `json:"updated_at"`
 }
 
 type Tasks struct {
@@ -23,7 +23,7 @@ func (t *Tasks) GetAll(c *gin.Context) {
 	tasks := []Task{}
 	res := t.Db.Order("created_at").Find(&tasks)
 
-	if res.RecordNotFound() {
+	if len(tasks) == 0 {
 		c.JSON(404, gin.H{"error": "no tasks in the table"})
 	} else {
 		c.JSON(200, gin.H{"data": res.Value})
@@ -45,7 +45,7 @@ func (t *Tasks) Get(c *gin.Context) {
 func (t *Tasks) Create(c *gin.Context) {
 	task := &Task{}
 
-	if c.Bind(task) == nil {
+	if c.BindJSON(task) == nil {
 		if 0 > task.Progression || 100 < task.Progression {
 			c.JSON(400, gin.H{"error": "task progression must be between 0 and 100"})
 		} else {
@@ -56,9 +56,11 @@ func (t *Tasks) Create(c *gin.Context) {
 			if res.Error != nil {
 				c.JSON(500, gin.H{"error": "Unable to create the task"})
 			} else {
-				c.JSON(201, gin.H{"data": task})
+				c.JSON(201, gin.H{"data": res.Value})
 			}
 		}
+	} else {
+		c.JSON(400, gin.H{"error": "Task must have a name"})
 	}
 }
 
@@ -66,7 +68,7 @@ func (t *Tasks) Update(c *gin.Context) {
 	updatedTask := &Task{}
 	id := c.Param("id")
 
-	if c.Bind(updatedTask) == nil {
+	if c.BindJSON(updatedTask) == nil {
 		if 0 > updatedTask.Progression || 100 < updatedTask.Progression {
 			c.JSON(400, gin.H{"error": "task progression must be between 0 and 100"})
 		} else {
--- task/task_test.go
+++ task/task_test.go
@@ -14,6 +14,9 @@ import (
 func TestTask(t *testing.T) {
 	is := assert.New(t)
 	db, _ := gorm.Open("sqlite3", "test.db")
+
+	db.DB()
+	db.SingularTable(true)
 	db.LogMode(true)
 
 	db.CreateTable(new(Task))
@@ -29,17 +32,58 @@ func TestTask(t *testing.T) {
 	resp := httptest.NewRecorder()
 	req, _ := http.NewRequest("GET", "/tasks", nil)
 	router.ServeHTTP(resp, req)
+	is.Equal(resp.Body.String(), "{\"error\":\"no tasks in the table\"}\n")
 	is.Equal(resp.Code, 404)
 
 	resp = httptest.NewRecorder()
 	req, _ = http.NewRequest("GET", "/task/1", nil)
 	router.ServeHTTP(resp, req)
+	is.Equal(resp.Body.String(), "{\"error\":\"no task found with id: 1\"}\n")
 	is.Equal(resp.Code, 404)
 
 	resp = httptest.NewRecorder()
-	reader := strings.NewReader("{ \"name\": \"first task\", \"description\":\"find some coffee\"}")
+	reader := strings.NewReader(`{ "name": "first task", "description": "find some coffee", "progression": 50 }`)
 	req, _ = http.NewRequest("POST", "/tasks", reader)
 	router.ServeHTTP(resp, req)
+	is.Equal(resp.Code, 201)
+
+	resp = httptest.NewRecorder()
+	req, _ = http.NewRequest("GET", "/task/1", nil)
+	router.ServeHTTP(resp, req)
+	is.Equal(resp.Code, 200)
+
+	resp = httptest.NewRecorder()
+	reader = strings.NewReader(`{ "name": "first task", "description": "find some coffee", "progression": 101}`)
+	req, _ = http.NewRequest("POST", "/tasks", reader)
+	router.ServeHTTP(resp, req)
+	is.Equal(resp.Code, 400)
+
+	resp = httptest.NewRecorder()
+	reader = strings.NewReader(`{ "name": "", "description": "find some coffee", "progression": 100}`)
+	req, _ = http.NewRequest("POST", "/tasks", reader)
+	router.ServeHTTP(resp, req)
+	is.Equal(resp.Body.String(), "{\"error\":\"Task must have a name\"}\n")
+	is.Equal(resp.Code, 400)
+
+	resp = httptest.NewRecorder()
+	reader = strings.NewReader(`{ "name": "updated task", "description": "find some coffee", "progression": 50 }`)
+	req, _ = http.NewRequest("PATCH", "/tasks/1", reader)
+	router.ServeHTTP(resp, req)
+	is.Equal(resp.Code, 201)
+
+	resp = httptest.NewRecorder()
+	reader = strings.NewReader(`{ "name": "updated task", "description": "find some coffee", "progression": -1 }`)
+	req, _ = http.NewRequest("PATCH", "/tasks/1", reader)
+	router.ServeHTTP(resp, req)
+	is.Equal(resp.Body.String(), "{\"error\":\"task progression must be between 0 and 100\"}\n")
+	is.Equal(resp.Code, 400)
+
+	resp = httptest.NewRecorder()
+	reader = strings.NewReader(`{ "name": "updated task", "description": "find some coffee", "progression": 101 }`)
+	req, _ = http.NewRequest("PATCH", "/tasks/1", reader)
+	router.ServeHTTP(resp, req)
+	is.Equal(resp.Body.String(), "{\"error\":\"task progression must be between 0 and 100\"}\n")
+	is.Equal(resp.Code, 400)
 
 	db.DropTable(&Task{})
 
